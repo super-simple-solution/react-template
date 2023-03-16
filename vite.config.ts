c835bd3f4c5d857b2940be2a15c0f5a061a82607
wuxue <wudaxue123@gmail.com>
2023-03-16 15:41:13 +0800

    tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, './src')}/`,
+    },
+  },
+  plugins: [
+    react()
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
 })
