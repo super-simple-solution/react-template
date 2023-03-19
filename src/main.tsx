9ef03e17c8665b0a0eca08353119248025f074cf
zhangpan <zhangpan@windimg.com>
2023-03-20 04:41:15 +0800

    toggle theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import '@/styles/index.less'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { ColorModeProvider } from '@chakra-ui/color-mode'
 import theme from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <Router>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <App />
-      </Router>
+    <ChakraProvider theme={theme}>
+      <ColorModeProvider>
+        <Router>
+          <App />
+        </Router>
+      </ColorModeProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
