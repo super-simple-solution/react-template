64a4eb7b04a681f6db762705fbe7d80b8f4b9b45
wuxue <1203588983@qq.com>
2023-03-20 18:51:51 +0800

    metamask

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import { Button } from '@chakra-ui/react'
 import { useMetaMask } from 'metamask-react'
 
 function App() {
-  const { status, connect, account, chainId, ethereum } = useMetaMask()
+  const { status, connect, account, chainId } = useMetaMask()
   if (status === 'initializing') return <div>Synchronisation with MetaMask ongoing...</div>
   if (status === 'unavailable') return <div>MetaMask not available :(</div>
   if (status === 'notConnected') return <button onClick={connect}>Connect to MetaMask</button>
