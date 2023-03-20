d1a047eebe8879b3044a07e4b828f668e85f4771
wuxue <wuxue@windimg.com>
2023-03-21 01:24:44 +0800

    storybook

--- src/layout/component/Header.tsx
+++ src/layout/component/Header.tsx
@@ -5,9 +5,9 @@ function Header() {
     <>
       <div className="header flex-x-between flex-y-center w-screen">
         <div>SingularDAO</div>
-        <div className="flex">
-          <div>Market</div>
-          <div>Dashboard</div>
+        <div className="flex-xy-center">
+          <div className="mr-80">Market</div>
+          <div className="mr-80">Dashboard</div>
           <div>AirDrop</div>
         </div>
         <div className="connect-btn">Connect Wallet</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        80: '80px',
+      },
+    },
   },
   plugins: [],
 }
